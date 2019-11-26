aebe34ce01aa7cdb0b294685c6e08f2a9ee2fe99
schang1146 <sammychang2185@gmail.com>
2019-11-25 20:41:15 -0800

    setup basic react-app

---
+++ schang1146.github.io/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styling/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Sammy Chang
+        Portfolio
+        Contact
+      </header>
+    </div>
+  );
+}
+
+export default App;
