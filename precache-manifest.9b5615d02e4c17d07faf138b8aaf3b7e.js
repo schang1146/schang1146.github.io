ddcc6724516c43b443c68ae4529f92ef5f11d496
schang1146 <sammychang2185@gmail.com>
2019-11-27 21:18:41 -0800

    Updates

---
+++ precache-manifest.9b5615d02e4c17d07faf138b8aaf3b7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e22bdfd80e64523f5b8f0246db36c9e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7b0aadef3f1f8ead3d9",
+    "url": "/static/css/main.0046f01f.chunk.css"
+  },
+  {
+    "revision": "dfb2107d63868035368e",
+    "url": "/static/js/2.6ae65415.chunk.js"
+  },
+  {
+    "revision": "e7b0aadef3f1f8ead3d9",
+    "url": "/static/js/main.f69b6cd6.chunk.js"
+  },
+  {
+    "revision": "5f874c4aaecf801cde50",
+    "url": "/static/js/runtime-main.7c2d13e6.js"
+  }
+]);
