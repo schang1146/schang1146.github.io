d46fb853b362e23deaf67d73926df8d8daa39a74
schang1146 <sammychang2185@gmail.com>
2020-03-07 19:54:51 -0800

    changed some colors

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { Button, ButtonGroup } from 'reactstrap';
+import { Button } from 'reactstrap';
 
 // import styling
 import './Header.scss';
@@ -15,26 +15,28 @@ class Header extends Component {
         return (
             <div id='header' className='header-wrapper'>
                 <div className='header-content'>
-                    <h1>Sammy Chang</h1>
-                    <hr />
+                    <h1>
+                        <text className='lighter'>Sammy</text>
+                        <br />
+                        Chang
+                    </h1>
                     <div className='header-nav'>
-                        <ButtonGroup>
-                            <Button outline color='secondary' size='lg' href='#about'>
-                                About
-                            </Button>
-                            <Button outline color='secondary' size='lg' href='#portfolio'>
-                                Portfolio
-                            </Button>
-                            <Button outline color='secondary' size='lg' href='#contact'>
-                                Contact
-                            </Button>
-                        </ButtonGroup>
+                        <Button outline color='link' size='lg' href='#about' className='header-links'>
+                            About
+                        </Button>
+                        <Button outline color='link' size='lg' href='#portfolio' className='header-links'>
+                            Portfolio
+                        </Button>
+                        <Button outline color='link' size='lg' href='#contact' className='header-links'>
+                            Contact
+                        </Button>
                     </div>
                     <div className='header-icons'>
                         <FontAwesomeIcon icon={faLinkedin} size='2x' className='icon' />
                         <FontAwesomeIcon icon={faEnvelope} size='2x' className='icon' />
                     </div>
                 </div>
+                <div className='header-images'></div>
             </div>
         );
     }
