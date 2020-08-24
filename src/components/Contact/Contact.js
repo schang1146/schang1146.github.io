31300bdeb50391a84370e7aa1f55cb3e45aaacaa
schang1146 <sammychang2185@gmail.com>
2020-08-24 16:57:10 -0700

    added border around social media icons; updating form with textarea

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -13,30 +13,37 @@ import { Button } from 'reactstrap';
 import './Contact.scss';
 
 class Contact extends Component {
+    sendMsg(e) {
+        e.preventDefault();
+        console.log('submit');
+    }
+
     render() {
         return (
             <div id='contact' className='contact-wrapper'>
                 <div className='contact-left'>
                     <h2 className='contact-title'>GET IN TOUCH</h2>
-                    <form className='contact-form'>
+                    <form className='contact-form' onSubmit={(e) => console.log('hello')}>
                         <span className='contact-form-top'>
                             <input type='text' name='yourName' id='yourName' placeholder='Name' />
                             <input type='email' name='yourEmail' id='yourEmail' placeholder='Email' />
                         </span>
                         <span className='contact-form-bottom'>
-                            <input type='textarea' name='yourMessage' id='yourMessage' placeholder='Message' />
+                            <textarea name='yourMessage' id='yourMessage' placeholder='Message' />
                         </span>
-                        <Button size='lg'>Send</Button>
+                        <Button className='custom-btn' onClick={(e) => this.sendMsg(e)}>
+                            SEND
+                        </Button>
                     </form>
                 </div>
                 <div className='contact-right'>
                     <h2 className='contact-title'>FOLLOW</h2>
                     <div className='contact-social-media'>
                         <a className='icon' href='https://www.github.com/schang1146/' target='_blank' rel='noopener noreferrer'>
-                            <FontAwesomeIcon icon={faGithub} className='icon' size='2x' />
+                            <FontAwesomeIcon icon={faGithub} size='2x' />
                         </a>
                         <a className='icon' href='https://www.linkedin.com/in/sammy-chang/' target='_blank' rel='noopener noreferrer'>
-                            <FontAwesomeIcon icon={faLinkedin} className='icon' size='2x' />
+                            <FontAwesomeIcon icon={faLinkedin} size='2x' />
                         </a>
                     </div>
                 </div>
