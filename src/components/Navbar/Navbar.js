2090dcb6a49ce8b7ddd39154d8acb00fe65f8c4c
schang1146 <sammychang2185@gmail.com>
2020-09-28 14:25:14 -0700

    finished navbar hamburger button animation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,12 +2,23 @@
 import React, { Component } from 'react';
 
 // import components
-import { Button } from 'reactstrap';
+// import { Button } from 'reactstrap';
 
 // import styling
 import './Navbar.scss';
 
 class Navbar extends Component {
+    constructor() {
+        super();
+        this.state = {
+            open: false,
+        };
+    }
+
+    toggle() {
+        this.setState({ open: !this.state.open });
+    }
+
     render() {
         return (
             <div className='navbar-wrapper'>
@@ -17,7 +28,7 @@ class Navbar extends Component {
                 {/* <div className='navbar-center'>
                     <text className='light'>Sammy&nbsp;</text>Chang
                 </div> */}
-                <div className='navbar-right'>
+                {/* <div className='navbar-right'>
                     <Button outline color='link' className='navbar-link'>
                         About
                     </Button>
@@ -27,6 +38,9 @@ class Navbar extends Component {
                     <Button outline color='link' className='navbar-link'>
                         Contact
                     </Button>
+                </div> */}
+                <div className={this.state.open ? 'menu-btn open' : 'menu-btn'} onClick={() => this.toggle()}>
+                    <div className='menu-btn__burger' />
                 </div>
             </div>
         );
