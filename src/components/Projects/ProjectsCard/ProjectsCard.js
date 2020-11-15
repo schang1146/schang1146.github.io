74a607141864397a451679137660278c14cb22ed
Sammy Chang <sammychang2185@gmail.com>
2020-11-15 14:52:19 -0800

    added pics to projects-card

--- src/components/Projects/ProjectsCard/ProjectsCard.js
+++ src/components/Projects/ProjectsCard/ProjectsCard.js
@@ -5,19 +5,24 @@ import React, { Component } from 'react';
 import './ProjectsCard.scss';
 
 // import images
-import h3rra from '../../../assets/images/h3rra.jpg';
+import test from '../../../assets/images/trivia-in-tandem/LandingView.png';
 
 class ProjectsCard extends Component {
     render() {
         console.log(this.props);
         return (
             <article className='projects-card-wrapper'>
-                <h3 className='projects-card-title'>{this.props.title.toUpperCase()}</h3>
-                {this.props.tags.map((tag) => {
-                    return <span className='projects-card-tag'>{tag}</span>;
-                })}
-                <div className='projects-card-description'>{this.props.description}</div>
-                <div className='projects-card-icons'></div>
+                <div className='projects-card-info'>
+                    <h3 className='projects-card-title'>{this.props.title.toUpperCase()}</h3>
+                    {this.props.tags.map((tag) => {
+                        return <span className='projects-card-tag'>{tag}</span>;
+                    })}
+                    <div className='projects-card-description'>{this.props.description}</div>
+                    <div className='projects-card-icons'></div>
+                </div>
+                <div className='projects-card-image'>
+                    <img src={test} alt=''></img>
+                </div>
             </article>
         );
     }
